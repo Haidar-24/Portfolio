f70fe6bb99d4688129319a8033d24c426f09fc4f
Haidar Abbas <haidarabbas382@gmail.com>
2025-03-08 13:18:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 8000
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/portfolio', (req,res) => {
+app.get('/', (req,res) => {
     res.render('index');
 })
 
